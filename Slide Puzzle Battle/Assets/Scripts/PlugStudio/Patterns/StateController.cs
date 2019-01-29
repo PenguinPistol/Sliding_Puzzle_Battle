ffa90eeabf929653e17560c5fb30bd9ef7b059cf
Songtler <Songtler@users.noreply.github.com>
2019-01-29 13:38:06 +0900

    Add State Change/Close Animation

--- Slide Puzzle Battle/Assets/Scripts/PlugStudio/Patterns/StateController.cs
+++ Slide Puzzle Battle/Assets/Scripts/PlugStudio/Patterns/StateController.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace com.PlugStudio.Patterns
 {
     public class StateController : Singleton<StateController>
     {
+        private const string STATE_CHANGE_ANIMATION = "StateChange";
+        private const string STATE_CLOSE_ANIMATION = "StateClose";
+
         [SerializeField]
         private List<State> stateList;
         private Dictionary<string, State> stateDictionary;
@@ -13,15 +17,12 @@ namespace com.PlugStudio.Patterns
         private string currentStateName;
         [SerializeField]
         private string beforeStateName;
-
-        public string CurrentStateName { get { return currentStateName; } }
-
         private State currentState;
 
-        public State CurrentState
-        {
-            get { return currentState; }
-        }
+        public Animator changeAnimation;
+
+        public string CurrentStateName { get { return currentStateName; } }
+        public State CurrentState { get { return currentState; } }
     
         private void Start()
         {
@@ -44,32 +45,16 @@ namespace com.PlugStudio.Patterns
     
         void Update()
         {
+            //Debug.Log(" : " + changeAnimation.GetCurrentAnimatorStateInfo(0).normalizedTime);
             if (currentState != null)
+            {
                 currentState.Execute();
+            }
         }
 
         public void ChangeState(string _name)
         {
-            beforeStateName = currentStateName;
-
-            if (currentState != null)
-            {
-                currentState.canvas.SetActive(false);
-                currentState.Exit();
-            }
-
-            currentState = stateDictionary[_name];
-
-            if (currentState == null)
-            {
-                Debug.Log(_name + " State is Not Found");
-                currentState = stateDictionary[beforeStateName];
-                beforeStateName = "";
-            }
-
-            currentState.canvas.SetActive(true);
-            currentState.Init();
-            currentStateName = currentState.GetType().Name;
+            StartCoroutine(Change(_name));
         }
 
         public void ChangeState(string _name, params object[] datas)
@@ -96,6 +81,60 @@ namespace com.PlugStudio.Patterns
 
         public void ChangeBeforeState(params object[] datas)
         {
+            changeAnimation.Play(STATE_CLOSE_ANIMATION, -1, 0f);
+
+            Debug.Log("" + changeAnimation.GetCurrentAnimatorStateInfo(0).normalizedTime);
+
+            StartCoroutine(ChangeBefore());
+        }
+
+        private IEnumerator Change(string _name)
+        {
+            float time = 0f;
+
+            changeAnimation.Play(STATE_CLOSE_ANIMATION);
+
+            while (time < changeAnimation.GetCurrentAnimatorStateInfo(0).length)
+            {
+                time += Time.deltaTime;
+                yield return null;
+            }
+
+            beforeStateName = currentStateName;
+
+            if (currentState != null)
+            {
+                currentState.canvas.SetActive(false);
+                currentState.Exit();
+            }
+
+            currentState = stateDictionary[_name];
+
+            if (currentState == null)
+            {
+                Debug.Log(_name + " State is Not Found");
+                currentState = stateDictionary[beforeStateName];
+                beforeStateName = "";
+            }
+
+            changeAnimation.Play(STATE_CHANGE_ANIMATION);
+            currentState.canvas.SetActive(true);
+            currentState.Init();
+            currentStateName = currentState.GetType().Name;
+        }
+
+        private IEnumerator ChangeBefore()
+        {
+            float time = 0f;
+
+            changeAnimation.Play(STATE_CLOSE_ANIMATION);
+
+            while (time < changeAnimation.GetCurrentAnimatorStateInfo(0).length)
+            {
+                time += Time.deltaTime;
+                yield return null;
+            }
+
             if (currentState != null)
             {
                 currentState.canvas.SetActive(false);
@@ -111,6 +150,7 @@ namespace com.PlugStudio.Patterns
                 beforeStateName = "";
             }
 
+            changeAnimation.Play(STATE_CHANGE_ANIMATION);
             currentState.canvas.SetActive(true);
             currentState.Init();
             currentStateName = currentState.GetType().Name;
